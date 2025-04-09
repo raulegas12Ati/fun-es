3f24943ef6140067b020265d5850ef36aad6c91c
Raul Egas <raul.sobrinho@eaportal.org>
2025-04-09 05:12:05 -0300

    primeiro commit do projeto - três exemplos concluidos

---
+++ main.js
@@ -0,0 +1,51 @@
+//função = bloco de código, usado para uma funcionalidade especifica que so é executado se for chamada
+
+/* 
+##exemplo 1
+function mensagem(){
+    alert("Seja bem-vindo(a)!")
+    console.log("Seja bem-vindo(a)!")
+    console.log("Você é muito legal!")
+}
+
+mensagem()
+*/
+
+/*
+##exemplo 2
+
+
+function somar(numero1, numero2){
+    console.log(numero1 + numero2)
+}
+
+let numero1 = 20
+let numero2 = 30
+
+somar(numero1, numero2)
+*/
+
+/* 
+##exemplo 3
+
+
+function raizQuadrada(numero){
+    console.log(Math.sqrt(numero))
+}
+
+function potenciacao(valor1, valor2){
+    console.log(Math.pow(valor1, valor2))
+}
+
+function multiplicacao(valor1, valor2){
+    console.log(valor1*valor2)
+}
+
+let numero1 = Number(prompt("Digite o número 1"))
+let numero2 = Number(prompt("Digite o número 2"))
+
+raizQuadrada(numero1)
+potenciacao(numero1, numero2)
+multiplicacao(numero2, numero1)
+*/
+
